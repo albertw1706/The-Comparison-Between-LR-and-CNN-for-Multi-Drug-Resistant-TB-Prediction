441efef957ae0277e94c366b9dce0a8043317434
albertw1706 <123548023+albertw1706@users.noreply.github.com>
2023-04-08 00:04:02 +0700

    Add files via upload

---
+++ MSA_code.py
@@ -0,0 +1,54 @@
+from Bio import SeqIO, AlignIO
+from Bio.Align import MultipleSeqAlignment
+
+# Define the region of interest
+rpoB_start = 759609
+rpoB_end = 763369
+
+fabG_inhA_start = 1673300
+fabG_inhA_end = 1675016
+
+katG_start = 2153235
+katG_end = 2156706
+
+ahpC_start = 2726087
+ahpC_end = 2726805
+
+pncA_start = 2288681
+pncA_end = 2289282
+
+embB_start = 4246514
+embB_end = 4249878
+
+file = open("lists.txt", "r")
+read_file = file.read()
+srr_accession = read_file.split("\n")
+print(len(srr_accession))
+
+def MSA(start, end):
+    seq_records = []
+    for accession in srr_accession:
+        file = accession + ".fa"
+        seq = next(SeqIO.parse(file, "fasta")).seq[start:end]
+        seq_records.append(SeqIO.SeqRecord(seq, id=accession, name="", description=""))
+    alignment = MultipleSeqAlignment(seq_records)
+    print (seq_records)
+    return alignment
+
+alignment = MSA(rpoB_start, rpoB_end)
+AlignIO.write(alignment, "rpoB.fasta", "fasta")
+
+alignment = MSA(fabG_inhA_start, fabG_inhA_end)
+AlignIO.write(alignment, "fabG_inhA.fasta", "fasta")
+
+alignment = MSA(katG_start, katG_end)
+AlignIO.write(alignment, "katG.fasta", "fasta")
+
+alignment = MSA(ahpC_start, ahpC_end)
+AlignIO.write(alignment, "ahpC.fasta", "fasta")
+
+alignment = MSA(pncA_start, pncA_end)
+AlignIO.write(alignment, "pncA.fasta", "fasta")
+
+alignment = MSA(embB_start, embB_end)
+AlignIO.write(alignment, "embB.fasta", "fasta")
