fad7fbb52d05d98c75c78bbf0cacf28c89afcd94
albertw1706 <123548023+albertw1706@users.noreply.github.com>
2023-05-27 15:14:28 +0700

    Update CNN_Preprocessing.py

--- CNN_Preprocessing.py
+++ CNN_Preprocessing.py
@@ -17,6 +17,7 @@ fasta_files = ["rpoB.fasta", "fabG_inhA.fasta", "katG.fasta", "ahpC.fasta", "pnc
 # Define a mapping of nucleotides to integers
 nucleotide_dict = {'A': 0, 'T': 1, 'G': 2, 'C': 3, '-': 4}
 
+# Function to one hot encode the nucelotides 
 def one_hot_encode(seq):
     """
     One-hot encode a DNA sequence with a gap character.
@@ -36,8 +37,7 @@ def one_hot_encode(seq):
 
     return one_hot
 
-data = {}
-
+# Form the 3D numpy arrays 
 for accession in srr_accession:
     sequences = []
     encoded_sequence = []
@@ -56,39 +56,42 @@ for accession in srr_accession:
                             encoded += "-"
                             if len(encoded) == 3888:
                                 break
-                        print (sequences)
                         if len(sequences) == 6:
                             one_encoded = one_hot_encode(encoded)   
                             encoded_sequence.append(one_encoded)   
-                            print (encoded_sequence)
                             sample_array = np.stack(encoded_sequence)
-                            print (sample_array)
                             sample_arrays.append(sample_array)
                         else :
                             pass
-print (sample_arrays)
 
+# Filter to select desired 3D array size
 selected_size = (6, 3888, 5)
 
+# Filter the list of arrays to only obtain the desired size
 filter_samples = np.where([samples.shape == selected_size for samples in sample_arrays])[0]
 filtered_list = [sample_arrays[i] for i in filter_samples]
 print (filtered_list)
 
+# Form the 4D array
 sample_4d_array = np.stack(filtered_list)
 sample_4d_array_shape = np.stack(filtered_list).shape
 
-print (sample_4d_array)
+#Check if the size are correct and all samples are included
 print (sample_4d_array_shape)
 
+#Reshape into the desired shape
 reshaped_4d_array = sample_4d_array.transpose(0, 3, 2, 1).reshape((12179, 5, 3888, 6))
 reshaped_4d_array_shape = sample_4d_array.transpose(0, 3, 2, 1).reshape((12179, 5, 3888, 6)).shape
 print (reshaped_4d_array)
 print (reshaped_4d_array_shape)
 
+# Save it into a pickle
 with open('CNN_input.pickle', 'wb') as f:
     pkl.dump(reshaped_4d_array, f)
 
+# Load the pickle file
 with open('CNN_input.pickle', 'rb') as f:
     loaded_data = pkl.load(f)
 
+# Check if the saved pickle file is correct
 print(np.array_equal(reshaped_4d_array, loaded_data))
